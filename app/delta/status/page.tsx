a6498165ace0efa0e30e8d1c3b1a2640085a32dc
wadder12 <apwade75009@gmail.com>
2024-08-12 16:07:43 -0500

    ✨ feat: added status page

---
+++ app/delta/status/page.tsx
@@ -0,0 +1,15 @@
+// app/page.tsx
+
+import Navbar from "../../components/Navbar";
+import Background from "../../components/Background";
+import Footer from "../../components/Footer";
+
+export default function Home() {
+  return (
+    <Background>
+      <Navbar />
+
+      <Footer />
+    </Background>
+  );
+}
